a57399c5b728aa0eaa6dcabff55598a686a31d00
Kevin-kinyanjui <kevin.kinyanjui@student.moringaschool.com>
2023-07-03 11:41:38 +0300

    assignment completed

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.include?('.')
   end
 
   def question?
-
+    self.include?('?')
   end
 
   def exclamation?
-
+    self.include?('!')
   end
 
   def count_sentences
-
+    self.split(/[.!?]/).reject(&:empty?).count
   end
 end
+
+puts "Random sentence. Is here.".sentence?
+puts "Random sentence. Is here?".question?
+puts "Random sentence. Is here!".exclamation?
+puts "Random sentence. Is here! Is here.".count_sentences
+
